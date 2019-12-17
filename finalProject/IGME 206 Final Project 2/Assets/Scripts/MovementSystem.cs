e8bd10f883938be91c173c6b861716e7243e4eb9
vg1566 <54686178+vg1566@users.noreply.github.com>
2019-12-17 12:19:37 -0500

    various

--- finalProject/IGME 206 Final Project 2/Assets/Scripts/MovementSystem.cs
+++ finalProject/IGME 206 Final Project 2/Assets/Scripts/MovementSystem.cs
@@ -1,4 +1,4 @@
-﻿using Unity.Burst;
+﻿/*using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
@@ -8,33 +8,41 @@ using static Unity.Mathematics.math;
 
 public class MovementSystem : JobComponentSystem
 {
-    // This declares a new kind of job, which is a unit of work to do.
-    // The job is declared as an IJobForEach<Translation, Rotation>,
-    // meaning it will process all entities in the world that have both
-    // Translation and Rotation components. Change it to process the component
-    // types you want.
-    //
-    // The job is also tagged with the BurstCompile attribute, which means
-    // that the Burst compiler will optimize it for the best performance.
+    
     [BurstCompile]
     struct MovementSystemJob : IJobForEach<Translation, Rotation>
     {
-        // Add fields here that your job needs to do its work.
-        // For example,
-        //    public float deltaTime;
-        
-        
-        
+        public float deltaTime;
+        public Transform Enemy;
+        float HP;
+        float MoveSpeed;
+
+
         public void Execute(ref Translation translation, [ReadOnly] ref Rotation rotation)
         {
-            // Implement the work to perform for each entity here.
-            // You should only access data that is local or that is a
-            // field on this job. Note that the 'rotation' parameter is
-            // marked as [ReadOnly], which means it cannot be modified,
-            // but allows this job to run in parallel with other jobs
-            // that want to read Rotation component data.
-            // For example,
-            //     translation.Value += mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
+            HP = 10;
+            MoveSpeed = 4;
+            if(gameObject.tag == "Tag1")
+            {
+                Enemy = GameObject.FindWithTag("Tag2").transform;
+                if (GameObject.FindWithTag("Tag2") != null)
+                {
+                    //Find and move towards enemy
+                    transform.LookAt(Enemy);
+                    transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+                }
+            }
+            else if(gameObject.tag == "Tag2")
+            {
+                Enemy = GameObject.FindWithTag("Tag2").transform;
+                if (GameObject.FindWithTag("Tag1") != null)
+                {
+                    //Find and move towards enemy
+                    transform.LookAt(Enemy);
+                    transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+                }
+            }
+            translation.Value += mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
             
             
         }
@@ -44,14 +52,7 @@ public class MovementSystem : JobComponentSystem
     {
         var job = new MovementSystemJob();
         
-        // Assign values to the fields on your job here, so that it has
-        // everything it needs to do its work when it runs later.
-        // For example,
-        //     job.deltaTime = UnityEngine.Time.deltaTime;
-        
-        
-        
-        // Now that the job is set up, schedule it to be run. 
+        job.deltaTime = UnityEngine.Time.deltaTime;
         return job.Schedule(this, inputDependencies);
     }
-}
+}*/
